18f6960c33d9df854f078a580c359e839ac83748
Rok Hudobivnik <rh8667@student.uni-lj.si>
2016-05-14 13:27:14 +0000

    random commit 1

--- streznik.js
+++ streznik.js
@@ -5,8 +5,25 @@ if (!process.env.PORT)
 var express = require('express');
 var streznik = express();
 var path = require("path");
+var expressSession = require('express-session');
+streznik.use(express.static('public'));
+streznik.use(
+  expressSession({
+    secret: 'NoDejNo', // Skrivni ključ za podpisovanje piškotkov
+    saveUninitialized: true,    // Novo sejo shranimo
+    resave: false,              // Ne zahtevamo ponovnega shranjevanja
+    cookie: {
+      maxAge: 3600000           // Seja poteče po 1h neaktivnosti
+    }
+  })
+);
+
+var glob=0;
+
+
 
 streznik.get('/', function (require, response) {
+    glob++;
     response.sendfile('index.html');
 });
 
