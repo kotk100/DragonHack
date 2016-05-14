f246027897c21c7d417e37ccbeee38ef02594899
Rok Hudobivnik <rh8667@student.uni-lj.si>
2016-05-14 13:15:11 +0000

    first commit (streznik)

---
+++ streznik.js
@@ -0,0 +1,15 @@
+if (!process.env.PORT)
+  process.env.PORT = 8080;
+
+// Priprava strežnika
+var express = require('express');
+var streznik = express();
+var path = require("path");
+
+streznik.get('/', function (require, response) {
+    response.sendfile('index.html');
+});
+
+streznik.listen(process.env.PORT, function() {
+  console.log("Strežnik pognan!");
+})
